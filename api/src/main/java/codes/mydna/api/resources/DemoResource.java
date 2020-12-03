0829fe1029e8393bc51071ea7d885827dfa45295
MusicDin <dm6067@student.uni-lj.si>
2020-12-03 18:24:44 +0100

    Demo resource

--- api/src/main/java/codes/mydna/api/resources/DemoResource.java
+++ api/src/main/java/codes/mydna/api/resources/DemoResource.java
@@ -7,6 +7,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @RequestScoped
 @Consumes(MediaType.APPLICATION_JSON)
@@ -15,87 +19,21 @@ import javax.ws.rs.core.Response;
 public class DemoResource {
 
     @GET
-    public Response demo(){
-        DemoObject demoObject = new DemoObject();
-        return Response.ok(demoObject).build();
+    public Response demo() {
+        StringBuilder content = new StringBuilder();
+
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("projectInfo.json")) {
+            if(inputStream != null){
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            }
+        } catch (IOException e) {
+            Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+        return Response.ok(content.toString()).build();
     }
-
-    class DemoObject {
-
-        private String[] clani;
-        private String opis_projekta;
-        private String[] mikrostoritve;
-        private String[] github;
-        private String[] travis;
-        private String[] jenkins;
-        private String[] dockerhub;
-
-        public DemoObject() {
-            clani = new String[]{"dm6067"};
-            opis_projekta = "Aplikacija za iskanje nizov v dna sekvencah in razne operazcije nad samimi sekvencami.";
-            // TODO: Fix analysis "mikrostoritve url"
-            mikrostoritve = new String[]{"https://sequence-bank.mydna.codes/v1/dna", "https://analysis.mydna.codes/v1/demo"};
-            github = new String[]{"https://github.com/mydna-codes/sequence-bank", "https://github.com/mydna-codes/analysis"};
-            jenkins = new String[]{"https://jenkins.din-cloud.com/job/mydnacodes/job/sequence-bank/", "https://jenkins.din-cloud.com/job/mydnacodes/job/analysis/"};
-            travis = jenkins.clone();
-            dockerhub = new String[]{"https://hub.docker.com/repository/docker/mydnacodes/sequence-bank", "https://hub.docker.com/repository/docker/mydnacodes/analysis"};
-        }
-
-        public String[] getClani() {
-            return clani;
-        }
-
-        public void setClani(String[] clani) {
-            this.clani = clani;
-        }
-
-        public String getOpis_projekta() {
-            return opis_projekta;
-        }
-
-        public void setOpis_projekta(String opis_projekta) {
-            this.opis_projekta = opis_projekta;
-        }
-
-        public String[] getMikrostoritve() {
-            return mikrostoritve;
-        }
-
-        public void setMikrostoritve(String[] mikrostoritve) {
-            this.mikrostoritve = mikrostoritve;
-        }
-
-        public String[] getGithub() {
-            return github;
-        }
-
-        public void setGithub(String[] github) {
-            this.github = github;
-        }
-
-        public String[] getTravis() {
-            return travis;
-        }
-
-        public void setTravis(String[] travis) {
-            this.travis = travis;
-        }
-
-        public String[] getJenkins() {
-            return jenkins;
-        }
-
-        public void setJenkins(String[] jenkins) {
-            this.jenkins = jenkins;
-        }
-
-        public String[] getDockerhub() {
-            return dockerhub;
-        }
-
-        public void setDockerhub(String[] dockerhub) {
-            this.dockerhub = dockerhub;
-        }
-    }
-
 }
