485e82e7a941036d117efc643433bef9dffbf7c0
MusicDin <dm6067@student.uni-lj.si>
2020-12-14 15:15:55 +0100

    Fixed info section in OAS3

--- api/src/main/java/codes/mydna/api/RestService.java
+++ api/src/main/java/codes/mydna/api/RestService.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -14,7 +16,11 @@ import javax.ws.rs.core.Application;
                 contact = @Contact(),
                 license = @License(),
                 description = "API for storing sequences like DNAs, genes and enzymes"
-        )
+        ),
+        servers = {
+                @Server(url = "https://sequence-bank-test.mydna.codes"),
+                @Server(url = "http://localhost:8080")
+        }
 )
 @ApplicationPath("v1")
 public class RestService extends Application {
