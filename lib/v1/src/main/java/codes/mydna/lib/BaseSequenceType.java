35dd769ead547158e90df7c5b8b716283dd78516
MusicDin <dm6067@student.uni-lj.si>
2020-12-15 02:41:26 +0100

    DTO field example value

--- lib/v1/src/main/java/codes/mydna/lib/BaseSequenceType.java
+++ lib/v1/src/main/java/codes/mydna/lib/BaseSequenceType.java
@@ -1,7 +1,10 @@
 package codes.mydna.lib;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 public class BaseSequenceType extends BaseType {
 
+    @Schema(description = "Sequence name", example = "My Sequence")
     private String name;
     private Sequence sequence;
 
--- lib/v1/src/main/java/codes/mydna/lib/Sequence.java
+++ lib/v1/src/main/java/codes/mydna/lib/Sequence.java
@@ -1,7 +1,10 @@
 package codes.mydna.lib;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 public class Sequence extends BaseType {
 
+    @Schema(description = "Sequence value", example = "ACTGAAAAGATATCCAGATACC")
     private String value;
 
     public String getValue() {
